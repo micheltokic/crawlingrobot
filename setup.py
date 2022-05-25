86f083faf0230d2271c13744d522faa593d5e49b
Michel Tokic <michel.tokic@siemens.com>
2022-05-25 13:37:10 +0200

    added version numbers to setup.py / adapted ipynb

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 
-setup(name='gym-crawlingrobot', version='1.0.0', install_requires = ['gym','pygame','pymunk', 'matplotlib', 'numpy'])
+setup(name='gym-crawlingrobot', version='1.0.0', install_requires = ['gym==0.21.0','pygame==2.1.2','pymunk==6.2.1', 'matplotlib==3.5.1', 'numpy==1.18.5', 'stable-baselines==2.10.2'])
